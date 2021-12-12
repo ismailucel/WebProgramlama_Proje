2913bb7eb04ea07a4ac1c892c49b221bff448079
İsmail Üçel <ismailucel@gmail.com>
2021-12-12 20:39:51 +0300

    Model Eklemeleri 1
    ApplicationUser, Artist, Category, Discount, ErrorViewModel, Photo modelleri eklendi.

---
+++ Proje Parçalar/Models/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G201210352.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        [NotMapped]
+        public string FullName { 
+            get
+            {
+                return Name + " " + LastName;
+            }
+               }
+
+    }
+}
---
+++ Proje Parçalar/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G201210352.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string SongName { get; set; }
+
+        public int? SongId { get; set; }
+        [ForeignKey("SongId")]
+        public Song Song { get; set; }
+    }
+}
