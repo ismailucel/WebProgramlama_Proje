550400e1662142dc518ec90f55f16ebd08e95552
İsmail Üçel <ismailucel@gmail.com>
2021-12-25 21:52:14 +0300

    Api İşlemleri
    StripeSettings.cs eklendi

---
+++ Proje Parçalar/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using G201210352.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G201210352.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        
+        public DbSet<Discount> Discount { get; set; }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Basket> Basket { get; set; }
+
+        public DbSet<BasketItem> BasketItem { get; set; }
+
+
+
+        public DbSet<Song> Song { get; set; }
+
+        public DbSet<Artist> Artist { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
